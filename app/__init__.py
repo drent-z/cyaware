026c1366a7ffc01ffc3d382d160e3506dad2c700
drent-z <45946041+drent-z@users.noreply.github.com>
2024-05-16 16:49:56 -0400

    fix templates

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ migrate = Migrate()
 bcrypt = Bcrypt()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     app.config['SECRET_KEY'] = 'your_secret_key'
 
