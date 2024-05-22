3675438b1264642bac6d92d997582e8cea7eb87d
drent-z <45946041+drent-z@users.noreply.github.com>
2024-05-22 16:28:56 -0500

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,15 @@
-# app/__init__.py
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
-import os
-
-db = SQLAlchemy()
-migrate = Migrate()
-bcrypt = Bcrypt()
+from .models import db
+from .routes import app as routes_app
 
 def create_app():
-    app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-    app.config['SECRET_KEY'] = 'your_secret_key'
-
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     db.init_app(app)
-    migrate.init_app(app, db)
-    bcrypt.init_app(app)
 
     with app.app_context():
-        from . import routes, models
         db.create_all()
+
+    app.register_blueprint(routes_app)
 
     return app
