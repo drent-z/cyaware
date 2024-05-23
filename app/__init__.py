0ab903221c8beb200cb7ca47a282d150f394ae94
drent-z <45946041+drent-z@users.noreply.github.com>
2024-05-23 16:45:43 -0500

    Fix syntax error and ensure app is defined

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,26 +10,17 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
 
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     with app.app_context():
-        from . import routes, models
-        db.create_all()
+        from . import routes, models  # Import routes and models
 
-    return app
+        db.create_all()  # Create database tables for our data models
 
-app = create_app()
+        return app
