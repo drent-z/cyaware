d9170b22496fdbe9346fe876259ff409c73657d5
drent-z <45946041+drent-z@users.noreply.github.com>
2024-05-23 20:51:55 -0500

    fixed nearly everything

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,26 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Auto fade out flash messages after 3 seconds
-    setTimeout(() => {
-        const flashMessages = document.querySelectorAll('.alert');
-        flashMessages.forEach((msg) => {
-            msg.classList.add('animate__fadeOutUp');
-            setTimeout(() => {
-                msg.remove();
-            }, 1000); // Ensure the fade-out animation completes before removing
-        });
-    }, 3000);
-
-    // Close button functionality
-    document.querySelectorAll('.alert .close').forEach((button) => {
-        button.addEventListener('click', (event) => {
-            const alert = event.target.closest('.alert');
-            alert.classList.add('animate__fadeOutUp');
-            setTimeout(() => {
-                alert.remove();
-            }, 1000); // Ensure the fade-out animation completes before removing
-        });
-    });
-
     // Toggle Navbar
     const navbarToggle = document.getElementById('navbar-toggle');
     const navLinks = document.getElementById('nav-links');
