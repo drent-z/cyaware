3b1077c27803e2b7bc073ba35aef4d8adc48cb94
drent-z <45946041+drent-z@users.noreply.github.com>
2024-05-24 09:40:33 -0500

    updated routes

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,3 +1,11 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user
+from .forms import RegisterForm, LoginForm
+from app.models import User
+from app import db, bcrypt
+
+users = Blueprint('users', __name__)
+
 @users.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -5,7 +13,7 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password_hash=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created!', 'success')
@@ -19,7 +27,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
             login_user(user, remember=form.remember_me.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('main.index'))
