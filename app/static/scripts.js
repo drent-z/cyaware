14390ff89b268c3bd34e8e2ebb6f35d52b1f4477
drent-z <45946041+drent-z@users.noreply.github.com>
2024-05-23 20:58:08 -0500

    fixes

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,13 +1,9 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Toggle Navbar
-    const navbarToggle = document.getElementById('navbar-toggle');
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('navbar-toggle');
     const navLinks = document.getElementById('nav-links');
 
-    navbarToggle.addEventListener('click', function() {
+    toggleButton.addEventListener('click', function () {
         navLinks.classList.toggle('active');
-        navLinks.classList.add('animate__animated', 'animate__fadeInDown');
-        setTimeout(() => {
-            navLinks.classList.remove('animate__animated', 'animate__fadeInDown');
-        }, 1000);
+        toggleButton.classList.toggle('open');
     });
 });
