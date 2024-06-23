eaf601cf1054f07c529827f39f9ee6d402e05919
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-23 15:56:34 -0400

    updated config

--- app/config.py
+++ app/config.py
@@ -1,14 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    
-    # Mail settings
-    MAIL_SERVER = os.environ.get('MAILGUN_SMTP_SERVER')
-    MAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    MAIL_SERVER = os.getenv('MAILGUN_SMTP_SERVER', 'smtp.mailgun.org')
+    MAIL_PORT = int(os.getenv('MAILGUN_SMTP_PORT', 587))
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAILGUN_SMTP_LOGIN')
-    MAIL_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
-    MAILGUN_DOMAIN = os.environ.get('MAILGUN_DOMAIN')
-    MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+    MAIL_USERNAME = os.getenv('MAILGUN_SMTP_LOGIN')
+    MAIL_PASSWORD = os.getenv('MAILGUN_SMTP_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'noreply@demo.com')
+    PAPERTRAIL_HOST = os.getenv('PAPERTRAIL_HOST', 'logs3.papertrailapp.com')
+    PAPERTRAIL_PORT = int(os.getenv('PAPERTRAIL_PORT', 12345))
