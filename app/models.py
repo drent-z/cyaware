db224b86ff210d55a8b45c5111edbad6963d8caf
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-23 23:54:32 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -16,21 +16,21 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        s = Serializer(current_app.config['SECRET_KEY'])
         return s.dumps({'user_id': self.id}).decode('utf-8')
     
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token, max_age=1800)['user_id']
+            user_id = s.loads(token)['user_id']
         except:
             return None
         return User.query.get(user_id)
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
+        
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
