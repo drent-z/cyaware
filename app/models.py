712576474afcbdaf2cf9d47e35031edefdb20d62
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-23 23:31:49 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import TimedSerializer as Serializer
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin
 from app import db, login_manager
@@ -14,24 +14,23 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    verified = db.Column(db.Boolean, default=False)
-
-    def get_verification_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expires_sec)
-        return s.dumps({'user_id': self.id})
+    
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
-    def verify_verification_token(token):
+    def verify_reset_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']
         except:
             return None
         return User.query.get(user_id)
-
+    
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
+        
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
