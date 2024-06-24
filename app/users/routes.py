403f7019f070e3e1c7957fbd6ee28192de6adcf9
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-24 16:50:48 -0400

    added account verification

--- app/users/routes.py
+++ app/users/routes.py
@@ -33,13 +33,17 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.index'))
-        else:
-            flash('Login unsuccessful. Please check email and password', 'danger')
+        if user:
+            if not user.is_verified:
+                flash('Account not verified. Please check your email to verify your account.', 'warning')
+                return redirect(url_for('users.login'))
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('main.index'))
+        flash('Login unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 @users.route("/logout")
 def logout():
