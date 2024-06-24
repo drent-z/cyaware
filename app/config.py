e45d35db49f7d2c8e28ba0a9f9c2f7eb364c8b14
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-23 22:50:38 -0400

    updated config

--- app/config.py
+++ app/config.py
@@ -1,14 +1,17 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()  # Load environment variables from .env file
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.getenv('MAILGUN_SMTP_SERVER', 'smtp.mailgun.org')
-    MAIL_PORT = int(os.getenv('MAILGUN_SMTP_PORT', 587))
+    MAIL_SERVER = os.getenv('MAILGUN_SMTP_SERVER')
+    MAIL_PORT = int(os.getenv('MAILGUN_SMTP_PORT'))
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv('MAILGUN_SMTP_LOGIN')
     MAIL_PASSWORD = os.getenv('MAILGUN_SMTP_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'noreply@demo.com')
-    PAPERTRAIL_HOST = os.getenv('PAPERTRAIL_HOST', 'logs3.papertrailapp.com')
-    PAPERTRAIL_PORT = int(os.getenv('PAPERTRAIL_PORT', 12345))
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    PAPERTRAIL_HOST = os.getenv('PAPERTRAIL_HOST')
+    PAPERTRAIL_PORT = int(os.getenv('PAPERTRAIL_PORT'))
