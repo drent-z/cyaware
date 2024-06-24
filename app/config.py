c76ede5b1f797b11ef71cb8656e3d12785c45cf4
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-24 00:37:43 -0400

    added debugging

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_hard_to_guess_string'
@@ -12,11 +15,10 @@ class Config:
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
     MAILGUN_DOMAIN = os.getenv('MAILGUN_DOMAIN')
     MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY')
+    MAILGUN_SMTP_LOGIN = os.getenv('MAILGUN_SMTP_LOGIN')
     PAPERTRAIL_HOST = os.getenv('PAPERTRAIL_HOST')
     PAPERTRAIL_PORT = int(os.getenv('PAPERTRAIL_PORT'))
 
-# Optional: Add logging to check if the variable is loaded correctly
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logging.debug(f'MAILGUN_API_KEY: {os.getenv("MAILGUN_API_KEY")}')
+logging.debug(f'MAILGUN_SMTP_LOGIN: {Config.MAILGUN_SMTP_LOGIN}')
+logging.debug(f'MAILGUN_API_KEY: {Config.MAILGUN_API_KEY}')
+logging.debug(f'MAILGUN_DOMAIN: {Config.MAILGUN_DOMAIN}')
--- app/users/utils.py
+++ app/users/utils.py
@@ -1,5 +1,6 @@
 import requests
 from flask import url_for, current_app
+import logging
 
 def save_picture(form_picture):
     # Function to save profile pictures (implementation not included in the provided snippet)
@@ -9,21 +10,26 @@ def send_reset_email(user):
     token = user.get_reset_token()
     subject = 'Password Reset Request'
     body = f'''To reset your password, visit the following link:
-{url_for('users.reset_token', token=token, _external=True)}
-If you did not make this request, then simply ignore this email and no changes will be made.
-'''
+    {url_for('users.reset_token', token=token, _external=True)}
+    If you did not make this request, then simply ignore this email and no changes will be made.
+    '''
     send_email(subject, user.email, body)
 
 def send_verification_email(user):
-    token = user.get_reset_token()
+    token = user.get_verification_token()
     subject = 'Account Verification'
     body = f'''To verify your account, visit the following link:
-{url_for('users.verify_email', token=token, _external=True)}
-If you did not make this request, then simply ignore this email.
-'''
+    {url_for('users.verify_email', token=token, _external=True)}
+    If you did not make this request, then simply ignore this email.
+    '''
     send_email(subject, user.email, body)
 
 def send_email(subject, to, body):
+    logging.debug(f'Sending email to {to} with subject: {subject}')
+    logging.debug(f'MAILGUN_DOMAIN: {current_app.config["MAILGUN_DOMAIN"]}')
+    logging.debug(f'MAILGUN_API_KEY: {current_app.config["MAILGUN_API_KEY"]}')
+    logging.debug(f'MAILGUN_SMTP_LOGIN: {current_app.config["MAILGUN_SMTP_LOGIN"]}')
+    
     response = requests.post(
         f"https://api.mailgun.net/v3/{current_app.config['MAILGUN_DOMAIN']}/messages",
         auth=("api", current_app.config['MAILGUN_API_KEY']),
