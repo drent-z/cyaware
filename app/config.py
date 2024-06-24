a7386a13345493f26b736154c40a007726edd584
drent-z <45946041+drent-z@users.noreply.github.com>
2024-06-23 23:00:13 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Load environment variables from .env file
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
