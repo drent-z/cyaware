b1de3aafdda7ca5bc2f1514591047231991e7690
drent-z <45946041+drent-z@users.noreply.github.com>
2024-07-12 15:51:25 -0400

    update

--- app/users/routes.py
+++ app/users/routes.py
@@ -155,12 +155,14 @@ def verify_token(token):
 @users.route("/contact", methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
-    recaptcha_site_key = current_app.config['RECAPTCHA_SITE_KEY']
+    recaptcha_site_key = os.getenv('RECAPTCHA_SITE_KEY')
+    recaptcha_secret_key = os.getenv('RECAPTCHA_SECRET_KEY')
+    
     if form.validate_on_submit():
-        recaptcha_token = request.form.get('recaptcha_token')
+        recaptcha_token = request.form.get('g-recaptcha-response')
         project_id = current_app.config['GOOGLE_CLOUD_PROJECT_ID']
         recaptcha_action = 'contact'
-
+        
         # Log form submission and reCAPTCHA token generation
         current_app.logger.info(f"Form submitted with reCAPTCHA token: {recaptcha_token}")
 
@@ -173,16 +175,14 @@ def contact():
             }
         }
 
-        # Log request body
-        current_app.logger.debug(f"reCAPTCHA request body: {request_body}")
-
         # Send the request to reCAPTCHA Enterprise API
         try:
             recaptcha_response = requests.post(
-                f'https://recaptchaenterprise.googleapis.com/v1/projects/{project_id}/assessments?key={current_app.config["RECAPTCHA_SECRET_KEY"]}',
+                f'https://recaptchaenterprise.googleapis.com/v1/projects/{project_id}/assessments?key={recaptcha_secret_key}',
                 json=request_body
             )
-            recaptcha_response.raise_for_status()  # Raise HTTPError for bad responses
+            recaptcha_response.raise_for_status()
+            current_app.logger.info("reCAPTCHA verification request sent successfully")
         except requests.exceptions.RequestException as e:
             current_app.logger.error(f"Error sending reCAPTCHA verification request: {e}")
             flash('Failed to verify reCAPTCHA. Please try again.', 'danger')
